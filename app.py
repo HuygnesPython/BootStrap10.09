5a48f48ec85d1268398e6e9122bf97a78fba0837
dionisio300 <dionisioassis80@gmail.com>
2025-09-10 21:59:04 -0300

    Não finalizado

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+from turtle import title
 from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/PagInicial')
+@app.route('/')
 def PagInicial():
-    titulo = 'Página inicial'
-    return render_template('index.html', titulo = titulo)
+    title = 'Página inicial'
+    return render_template('index.html', title=title)
+@app.route('/atividade')
+def atividade():
+    title = 'Atividade'
+    return render_template('atividade.html', title=title)
+@app.route('/atividade2')
+def atividade2():
+    title = 'Atividade 2'
+    return render_template('atividade2.html', title=title)
+
 
 app.run(debug=True)
